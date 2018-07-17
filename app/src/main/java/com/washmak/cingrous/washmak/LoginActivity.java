c370424a7e12631e445460983469571de87a5e2a
sanjaysanju618 <sanjaybabu618@gmail.com>
2018-07-17 18:34:51 +0530

    login page to workerdash

--- app/src/main/java/com/washmak/cingrous/washmak/LoginActivity.java
+++ app/src/main/java/com/washmak/cingrous/washmak/LoginActivity.java
@@ -1,13 +1,35 @@
 package com.washmak.cingrous.washmak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+/*
+    Button btn_login_from_login, btn_google_login_from_login;
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        findViewById(R.id.btn_login_from_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, WorkerdashbordActvity.class));
+            }
+        });
+
+        findViewById(R.id.btn_google_login_from_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, WorkerdashbordActvity.class));
+            }
+        });
+
     }
 }
