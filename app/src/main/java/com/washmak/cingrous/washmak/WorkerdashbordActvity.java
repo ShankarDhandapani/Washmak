8e610f91bb7d8ea698db20e2a10d2530c31fb1a0
sanjaysanju618 <sanjaybabu618@gmail.com>
2018-07-17 18:51:11 +0530

    simple list check

--- app/src/main/java/com/washmak/cingrous/washmak/WorkerdashbordActvity.java
+++ app/src/main/java/com/washmak/cingrous/washmak/WorkerdashbordActvity.java
@@ -2,6 +2,11 @@ package com.washmak.cingrous.washmak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WorkerdashbordActvity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class WorkerdashbordActvity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workerdashbord_actvity);
+
+        final ListView listview = findViewById(R.id.lv_from_workeract);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+        final ArrayList<String> list = new ArrayList<>(Arrays.asList(values));
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(WorkerdashbordActvity.this, android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
     }
 }
