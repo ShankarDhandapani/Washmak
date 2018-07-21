917ff41c579a00e23b4ecc981a9c6bc67e792b1a
sanjaysanju618 <sanjaybabu618@gmail.com>
2018-07-21 17:23:08 +0530

    Added Manager and Worker Login

--- app/src/main/java/com/washmak/cingrous/washmak/LoginActivity.java
+++ app/src/main/java/com/washmak/cingrous/washmak/LoginActivity.java
@@ -1,35 +1,81 @@
 package com.washmak.cingrous.washmak;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
-/*
-    Button btn_login_from_login, btn_google_login_from_login;
-*/
+    private FirebaseAuth mAuth;
+    private EditText email_from_login, password_from_login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
+        email_from_login = findViewById(R.id.et_email_from_login);
+        password_from_login = findViewById(R.id.et_pass_from_login);
 
         findViewById(R.id.btn_login_from_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, WorkerdashbordActvity.class));
+
+                String email = email_from_login.getText().toString().trim();
+                String pass = password_from_login.getText().toString().trim();
+
+                if (!email.equals("") || !pass.equals("")) {
+                    mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                assert user != null;
+                                updateUI(user);
+                            } else {
+                                updateUI(null);
+                            }
+                        }
+                    });
+                }else {
+                    Toast.makeText(LoginActivity.this, "Email and Password can't be empty", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
-        findViewById(R.id.btn_google_login_from_login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, WorkerdashbordActvity.class));
-            }
-        });
 
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentuser = mAuth.getCurrentUser();
+        if(currentuser != null)
+            updateUI(currentuser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        String Uid = currentUser.getUid();
+        if (Uid.equals("c1LaIw9sU2hc51w3FsZ2BS47HLt1")){
+            startActivity(new Intent(LoginActivity.this, ManagerActivity.class));
+        }else {
+            startActivity(new Intent(LoginActivity.this, WorkerdashbordActvity.class));
+        }
+        finish();
     }
 }
