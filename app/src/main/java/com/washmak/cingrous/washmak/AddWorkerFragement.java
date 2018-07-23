11f8121492440b3e99ada518a4f3c00ae0b7a241
sanjaysanju618 <sanjaybabu618@gmail.com>
2018-07-23 15:38:13 +0530

    Added Add Worker IN Manager

--- app/src/main/java/com/washmak/cingrous/washmak/AddWorkerFragement.java
+++ app/src/main/java/com/washmak/cingrous/washmak/AddWorkerFragement.java
@@ -1,5 +1,8 @@
 package com.washmak.cingrous.washmak;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,16 +10,86 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.Objects;
 
 public class AddWorkerFragement extends Fragment {
+    final Calendar c = Calendar.getInstance();
+    int hr = c.get(Calendar.HOUR_OF_DAY);
+    int min = c.get(Calendar.MINUTE);
+    TextView from_time, to_time;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragement_add_worker, container, false);
+
+        final View view = inflater.inflate(R.layout.fragement_add_worker, container, false);
+        from_time = view.findViewById(R.id.time_from_from_add_details);
+        to_time = view.findViewById(R.id.time_to_from_add_details);
+
+
+        view.findViewById(R.id.set_time_from_from_add_details).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        updateTime(hourOfDay, minute, 1);
+                    }
+                }, hr, min, false);
+                timePickerDialog.show();
+            }
+        });
+
+        view.findViewById(R.id.set_time_to_from_add_details).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        updateTime(hourOfDay, minute, 0);
+                    }
+                }, hr, min, false);
+                timePickerDialog.show();
+            }
+        });
+        return view;
+
     }
+
+    private void updateTime(int hours, int mins, int i) {
+        String timeSet = "";
+    if (hours > 12) {
+        hours -= 12;
+        timeSet = "PM";
+    } else if (hours == 0) {
+        hours += 12;
+        timeSet = "AM";
+    } else if (hours == 12)
+        timeSet = "PM";
+    else
+        timeSet = "AM";
+        String minutes = "";
+        if (mins < 10)
+            minutes = "0" + mins;
+        else
+            minutes = String.valueOf(mins);
+        String aTime = String.valueOf(hours) + ':' + minutes + " " + timeSet;
+
+        if(i == 1){
+            from_time.setText(aTime);
+        }else {
+            to_time.setText(aTime);
+
+        }
+    }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
